ba1bfff2aa70ed63ca2d8fe0edaf31edfd65dc4b
Just Winter <95353970+CrimsonDisk@users.noreply.github.com>
2025-04-06 15:38:00 +0700

    Add files via upload

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,16 +1,15 @@
+//Swiper scripts
 const swiper = new Swiper('.slider-wrapper', {
     loop: true,
     grabCursor: true,
     spaceBetween: 80,
 
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true,
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
